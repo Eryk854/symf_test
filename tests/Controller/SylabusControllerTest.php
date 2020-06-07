a22dc9c9c68261495968ec18c2f64d58c392f2cb
Eryk <eryk658@wp.pl>
2020-06-07 18:38:52 +0200

    Dodanie testow dotyczacych tworzenia/duplikacji sylabusow

--- tests/Controller/SylabusControllerTest.php
+++ tests/Controller/SylabusControllerTest.php
@@ -53,13 +53,12 @@ class SylabusControllerTest extends WebTestCase
             'PHP_AUTH_PW'   => 'manager1',
         ));
         //$client->request('GET', '/sylabus/form/1');
-        $crawler = $client->request('GET', '/sylabus/form/29');
+        $crawler = $client->request('GET', '/sylabus/form/30');
         $form = $crawler->selectButton('sylabus[confirm]')->form();
+        $crawler = $client->submit($form);
 
-        #$crawler = $client->submit($form);
-        $this->assertEquals($client->request('GET','/sylabus/form/29')->getUri(), $form->getUri());
-        #$this->assertEquals(302, $client->getResponse()->getStatusCode());
-        #$this->assertTrue($client->getResponse()->isRedirect('/sylabus/29'));
+        $this->assertEquals(302, $client->getResponse()->getStatusCode());
+        $this->assertTrue($client->getResponse()->isRedirect('/sylabus/30'));
 
     }
     public function testSaveSylabus()
@@ -125,15 +124,61 @@ class SylabusControllerTest extends WebTestCase
         $crawler = $client->submitForm('nowy_sylabus[utworz]', ['nowy_sylabus[nazwa]' => 'Analiza']);
         $crawler_not = $client->submitForm('nowy_sylabus[utworz]', ['nowy_sylabus[nazwa]' => 'wfefwe']);
 
-        #$crawler = $client->request('POST', '/sylabus/new/2',['nazwa'=>'Analiza']);
+
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
         $this->assertEquals('Utwórz sylabus o tej nazwie bez duplikacji żadnych danych',$crawler->selectButton('without_duplication')->text());
         $this->assertGreaterThan(6, $crawler->filter('button')->count());
         $this->assertEquals(3, $crawler_not->filter('button')->count());
     }
+    public function testDuplication()
+    {
+        $client = static::createClient(array(), array(
+            'PHP_AUTH_USER' => 'manager1',
+            'PHP_AUTH_PW'   => 'manager1',
+        ));
+        $client->request('POST', '/sylabus/make/',['program_id'=>2,'przedmiot_id'=>'3']);
+        $this->assertEquals(302, $client->getResponse()->getStatusCode());
 
+        $crawler=$client->followRedirect();
+        $form = $crawler->selectButton('sylabus[save]')->form();
+        $godziny = $form->get('sylabus[zajecia][godziny][godziny_wykladowe]')->getValue();
+        $kryteria = $form->get('sylabus[zajecia][kryteria_oceniania]')->getValue();
+        // Sprawdzamy czy sylabus skopiował się poprawnie
+        $this->assertStringContainsString('Podstawy fizyki',$crawler->filter('h2')->eq(0)->text());
+        $this->assertEquals('30',$godziny);
+        $this->assertEquals('Ćwiczenia i kolokwium pisemne 40%, egzamin pisemny 60%.',$kryteria);
 
+        //Sprawdzamy czy wartości niezbędne do wyświetlenia podglądu sylabusa zostały poprawnie zapisane
+        $nr = substr($crawler->getUri(),-3);
+        $client->request('GET', '/sylabus/'.$nr);
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+    }
+    public function testNewWithoutDuplication()
+    {
+        $client = static::createClient(array(), array(
+            'PHP_AUTH_USER' => 'manager1',
+            'PHP_AUTH_PW'   => 'manager1',
+        ));
 
+        $client->request('POST', '/sylabus/make/',['program_id'=>2, 'nazwa'=>'Programowanie komponentowe']);
+        $this->assertEquals(302, $client->getResponse()->getStatusCode());
+        $crawler=$client->followRedirect();
+        $form = $crawler->selectButton('sylabus[save]')->form();
+        $godziny_wyk = $form->get('sylabus[zajecia][godziny][godziny_wykladowe]')->getValue();
+        $godziny_proj = $form->get('sylabus[zajecia][godziny][godziny_projektowe]')->getValue();
+        $kryteria = $form->get('sylabus[zajecia][kryteria_oceniania]')->getValue();
+
+        // Testujemy czy wyświetlił się odpowiedni formularz do edycji nowego sylabusa
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertStringContainsString('Programowanie komponentowe',$crawler->filter('h2')->eq(0)->text());
+        $this->assertEquals('0',$godziny_wyk);
+        $this->assertEquals('',$kryteria);
+
+        //Testujemy czy wartości inicjalizujące zostały dobrze ustawione i czy podgląd sylabusa poprawnie się wyświetla
+        $nr = substr($crawler->getUri(),-3);
+        $client->request('GET', '/sylabus/'.$nr);
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+    }
 
 }
